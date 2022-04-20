554f8846baf8fc51b89ff1d68284c8c187e3d30d
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-04-20 13:55:31 +0000

    Finished
    Finished but need to return without TODO

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:destini/story_brain.dart';
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -28,9 +27,7 @@ class _StoryPageState extends State<StoryPage> {
         //TODO: Step 1 - Add background.png to this Container as a background image.
         decoration: BoxDecoration(
           image: DecorationImage(
-              image: AssetImage('images/background.png'),
-          fit: BoxFit.cover
-          ),
+              image: AssetImage('images/background.png'), fit: BoxFit.cover),
         ),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
@@ -52,20 +49,25 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-                    storyBrain.nextStory(1);
-                  },
-                  color: Colors.red,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     storyBrain.getChoice1(),
                     style: TextStyle(
+                      color: Colors.white,
                       fontSize: 20.0,
                     ),
                   ),
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
+                  },
                 ),
               ),
               SizedBox(
@@ -75,19 +77,27 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                    storyBrain.nextStory(2);
-                  },
-                  color: Colors.blue,
-                  child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    storyBrain.getChoice2(),
-                    style: TextStyle(
-                      fontSize: 20.0,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.blue,
                     ),
+                    child: Text(
+                      //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
                   ),
                 ),
               ),
